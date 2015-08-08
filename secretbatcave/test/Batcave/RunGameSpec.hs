2b26b71c9e8db27823158b0d2b65f687812850bc
Christopher Chen <chris@openendings.net>
2015-08-08 19:27:38 +1000

    Empty test module for RunGame

---
+++ secretbatcave/test/Batcave/RunGameSpec.hs
@@ -0,0 +1,10 @@
+module Batcave.RunGameSpec where
+
+import Test.Hspec
+import Test.QuickCheck
+import Test.QuickCheck.Property
+import Batcave.RunGame
+
+spec :: Spec
+spec = describe "Tests for Batcave.RunGame (command interpreting logic)" $ do
+  it "placeholder" $ property $ 1===1
