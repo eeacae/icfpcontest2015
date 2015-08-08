f2854334662c6b56b4cc6e6890502c871e1dbe5f
James Hales <james@jameshales.org>
2015-08-08 13:35:48 +1000

    Implement translate and rotate on Cells and Units
    Operations are implemented via a conversion to a cubic coordinate system
    described in <http://www.redblobgames.com/grids/hexagons/>.

--- secretbatcave/src/Batcave/Hex.hs
+++ secretbatcave/src/Batcave/Hex.hs
@@ -7,18 +7,8 @@ import           Batcave.Types
 
 ------------------------------------------------------------------------
 
--- | Identifies a cell in terms of a hexagonal coordinate grid system.
--- (0, 0) corresponds to the board's (0, 0).
--- (e, se) denotes e steps east, then se steps southest, from the board's (0,0).
-data AbsoluteCell = AbsoluteCell !Int !Int
-
-toAbs :: Cell -> AbsoluteCell
-toAbs (Cell x y) = AbsoluteCell (x - (y `div` 2)) y
-
-fromAbs :: AbsoluteCell -> Cell
-fromAbs (AbsoluteCell e se) = Cell (e + (se `div` 2)) se
-
--- TODO: QuickCheck that toAbs and fromAbs are inverses.
+-- | A hexagonal cubic coordinate
+data Cubic = Cubic !Int !Int !Int
 
 -- | An empty board of a width @w@ and height @h@.
 emptyBoard :: Int -> Int -> Board
@@ -49,7 +39,108 @@ unitUnoccupied b u = all (not . occupied b) $ unitMembers u
 unitPlaceable :: Board -> Unit -> Bool
 unitPlaceable b u = unitInBounds b u && unitUnoccupied b u
 
--- | Place a unit on a board. Return @Nothing@ if the placement is invalid.
-placeUnit :: Unit -> Board -> Maybe Board
-placeUnit u b | unitPlaceable b u = Just $ b // [(c, Full) | c <- V.toList (unitMembers u)]
-              | otherwise         = Nothing
+-- | Map over the cells in a unit.
+mapUnit :: (Cell -> Cell) -> Unit -> Unit
+mapUnit f u = u {
+  unitMembers = fmap f $ unitMembers u,
+  unitPivot   = f $ unitPivot u
+}
+
+-- | Convert a Cell to a Cubic. Taken from <http://www.redblobgames.com/grids/hexagons/>
+cellToCubic :: Cell -> Cubic
+cellToCubic (Cell col row) = Cubic x y z
+  where x = col - (row - (abs row `mod` 2)) `div` 2
+        z = row
+        y = -x - z
+
+-- | Convert a Cubic to a Cell.
+cubicToCell :: Cubic -> Cell
+cubicToCell (Cubic x y z) = Cell col row
+  where col = x + (z - (abs z `mod` 2)) `div` 2
+        row = z
+
+-- | Translate a Cubic 1 space East
+translateCubicEast :: Cubic -> Cubic
+translateCubicEast (Cubic x y z) = Cubic (x + 1) (y - 1) z
+
+-- | Translate a Cubic 1 space West
+translateCubicWest :: Cubic -> Cubic
+translateCubicWest (Cubic x y z) = Cubic (x - 1) (y + 1) z
+
+-- | Translate a Cubic 1 space East
+translateCubicSouthEast :: Cubic -> Cubic
+translateCubicSouthEast (Cubic x y z) = Cubic x (y - 1) (z + 1)
+
+-- | Translate a Cubic 1 space West
+translateCubicSouthWest :: Cubic -> Cubic
+translateCubicSouthWest (Cubic x y z) = Cubic (x - 1) y (z + 1)
+
+-- | Rotate a Cubic vector clockwise
+rotateCubicVectorCW :: Cubic -> Cubic
+rotateCubicVectorCW (Cubic x y z) = Cubic (-z) (-x) (-y)
+
+-- | Rotate a Cubic vector counter-clockwise
+rotateCubicVectorCCW :: Cubic -> Cubic
+rotateCubicVectorCCW (Cubic x y z) = Cubic (-y) (-z) (-x)
+
+-- | Rotate a Cubic clockwise around a central Cubic
+rotateCubicCW :: Cubic -> Cubic -> Cubic
+rotateCubicCW centre@(Cubic x' y' z') cell@(Cubic x y z) = Cubic (x' + x'') (y' + y'') (z' + z'')
+  where (Cubic x'' y'' z'') = rotateCubicVectorCW $ Cubic (x - x') (y - y') (z - z')
+
+-- | Rotate a Cubic counter clockwise around a central Cubic
+rotateCubicCCW :: Cubic -> Cubic -> Cubic
+rotateCubicCCW centre@(Cubic x' y' z') cell@(Cubic x y z) = Cubic (x' + x'') (y' + y'') (z' + z'')
+  where (Cubic x'' y'' z'') = rotateCubicVectorCCW $ Cubic (x - x') (y - y') (z - z')
+
+-- | Perform operation on a Cell via a conversion to Cubic.
+cubicOp :: (Cubic -> Cubic) -> Cell -> Cell
+cubicOp f = cubicToCell . f . cellToCubic
+
+-- | Translate a Cell 1 space East
+translateCellEast :: Cell -> Cell
+translateCellEast = cubicOp translateCubicEast
+
+-- | Translate a Cell 1 space East
+translateCellWest :: Cell -> Cell
+translateCellWest = cubicOp translateCubicWest
+
+-- | Translate a Cell 1 space East
+translateCellSouthEast :: Cell -> Cell
+translateCellSouthEast = cubicOp translateCubicSouthEast
+
+-- | Translate a Cell 1 space East
+translateCellSouthWest :: Cell -> Cell
+translateCellSouthWest = cubicOp translateCubicSouthWest
+
+-- | Rotate a Cell clockwise
+rotateCellCW :: Cell -> Cell -> Cell
+rotateCellCW centre cell = cubicOp (rotateCubicCW $ cellToCubic centre) cell
+
+-- | Rotate a Cell counter clockwise
+rotateCellCCW :: Cell -> Cell -> Cell
+rotateCellCCW centre cell = cubicOp (rotateCubicCCW $ cellToCubic centre) cell
+
+-- | Translate a Unit 1 space East
+translateUnitEast :: Unit -> Unit
+translateUnitEast = mapUnit translateCellEast
+
+-- | Translate a Unit 1 space West
+translateUnitWest :: Unit -> Unit
+translateUnitWest = mapUnit translateCellWest
+
+-- | Translate a Unit 1 space SouthEast
+translateUnitSouthEast :: Unit -> Unit
+translateUnitSouthEast = mapUnit translateCellEast
+
+-- | Translate a Unit 1 space SouthWest
+translateUnitSouthWest :: Unit -> Unit
+translateUnitSouthWest = mapUnit translateCellWest
+
+-- | Rotate a Unit clockwise
+rotateUnitCW :: Unit -> Unit
+rotateUnitCW u = mapUnit (rotateCellCW $ unitPivot u) u
+
+-- | Rotate a Unit counter clockwise
+rotateUnitCCW :: Unit -> Unit
+rotateUnitCCW u = mapUnit (rotateCellCCW $ unitPivot u) u
