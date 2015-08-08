f1587de0e8f591e4cd380a223d6c5ed5f7cd6c5e
Jacob Stanley <jacob@stanley.io>
2015-08-08 11:22:29 +1000

    Problems including json decode

--- secretbatcave/src/Batcave/Hex.hs
+++ secretbatcave/src/Batcave/Hex.hs
@@ -1,77 +1,33 @@
 module Batcave.Hex where
 
-import Data.Array
+import           Data.Array
+import qualified Data.Vector as V
 
--- | An initial game configuration and information on how many games to play.
-data Game = Game {
-  -- | A unique number identifying the problem
-  id :: Int,
-  {- |
-   - The various unit configurations that may appear in this game.
-   - There might be multiple entries for the same unit.
-   - When a unit is spawned, it will start off in the orientation
-   - specified in this field.
-   -}
-  units :: [Unit],
-  -- | The number of cells in a row
-  width :: Int,
-  -- | The number of rows on the board
-  height :: Int,
-  -- | Which cells start filled
-  filled:: [Cell],
-  -- | How many units in the source
-  sourceLength:: Int,
-  -- | How to generate the source and how many games to play
-  sourceSeeds:: [Int]
-}
+import           Batcave.Types
 
--- | The configuration of a unit. The cells are relative to a local coordinate
--- system, and will be translated to the board when the unit is spawned. The
--- local coordinate system of each cell, like the board's coordinate system,
--- has smaller row numbers in the "up" direction and smaller column numbers in
--- the "left" direction.
-data Unit = Unit {
-  -- | The unit members
-  members :: [Cell],
-  -- | The rotation point of the unit
-  pivot :: Cell
-}
-
--- | Identifies a cell, either on the board or within a unit.
-type Cell = (Int, Int)
+------------------------------------------------------------------------
 
 -- | Identifies a cell in terms of a hexagonal coordinate grid system.
 -- (0, 0) corresponds to the board's (0, 0).
 -- (e, se) denotes e steps east, then se steps southest, from the board's (0,0).
-newtype AbsoluteCell = AbsoluteCell (Int, Int)
+data AbsoluteCell = AbsoluteCell !Int !Int
 
 toAbs :: Cell -> AbsoluteCell
-toAbs (x, y) = AbsoluteCell (x - (y `div` 2), y)
+toAbs (Cell x y) = AbsoluteCell (x - (y `div` 2)) y
 
 fromAbs :: AbsoluteCell -> Cell
-fromAbs (AbsoluteCell (e, se)) = (e + (se `div` 2), se)
+fromAbs (AbsoluteCell e se) = Cell (e + (se `div` 2)) se
 
 -- TODO: QuickCheck that toAbs and fromAbs are inverses.
-
--- | Whether a cell is full or empty.
-data CellStatus = Full | Empty
-  deriving (Eq, Ord, Show)
-
--- | A game board. The game board consists of hexagonal cells arranged in rows,
--- with the first row numbered 0. The cells in a row are oriented so that they
--- have vertices up and down, and edges to the left and right. The first cell
--- in a row is numbered 0, thus each cell may be identified by a pair of
--- coordinates (column, row).
-type Board = Array Cell CellStatus
 
 -- | An empty board of a width @w@ and height @h@.
 emptyBoard :: Int -> Int -> Board
 emptyBoard w h = array boundingCells [(c, Empty) | c <- range boundingCells]
-  where boundingCells = ((0, 0), (w, h))
+  where boundingCells = (Cell 0 0, Cell w h)
 
 -- | Create the initial board described by a game configuration.
-initialBoard :: Game -> Board
-initialBoard g = emptyBoard (width g) (height g) // [(c, Full) | c <- filled g]
+initialBoard :: Problem -> Board
+initialBoard p = emptyBoard (problemWidth p) (problemHeight p) // [(c, Full) | c <- V.toList (problemFilled p)]
 
 -- | Test whether a cell is within the bounds of a board.
 inBounds :: Board -> Cell -> Bool
@@ -79,7 +35,7 @@ inBounds = inRange . bounds
 
 -- | Test whether all of the members of a unit are within the bounds of a board.
 unitInBounds :: Board -> Unit -> Bool
-unitInBounds b u = all (inBounds b) $ members u
+unitInBounds b u = all (inBounds b) $ unitMembers u
 
 -- | Test whether a cell is occupied on a board.
 occupied :: Board -> Cell -> Bool
@@ -87,7 +43,7 @@ occupied b c = b ! c == Full
 
 -- | Test whether all of the members of a unit are unoccupied.
 unitUnoccupied :: Board -> Unit -> Bool
-unitUnoccupied b u = all (not . occupied b) $ members u
+unitUnoccupied b u = all (not . occupied b) $ unitMembers u
 
 -- | Test whether a unit can be placed on a board.
 unitPlaceable :: Board -> Unit -> Bool
@@ -95,5 +51,5 @@ unitPlaceable b u = unitInBounds b u && unitUnoccupied b u
 
 -- | Place a unit on a board. Return @Nothing@ if the placement is invalid.
 placeUnit :: Unit -> Board -> Maybe Board
-placeUnit u b | unitPlaceable b u = Just $ b // [(c, Full) | c <- members u]
+placeUnit u b | unitPlaceable b u = Just $ b // [(c, Full) | c <- V.toList (unitMembers u)]
               | otherwise         = Nothing
---
+++ secretbatcave/src/Batcave/Types.hs
@@ -0,0 +1,134 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Batcave.Types where
+
+import Control.Monad (mzero)
+import Data.Aeson (FromJSON(..), Value(..), (.:))
+import Data.Array (Array)
+import Data.Vector (Vector)
+import GHC.Arr (Ix(..))
+
+------------------------------------------------------------------------
+
+-- | Identifies a cell, either on the board or within a unit.
+data Cell = Cell !Int !Int
+  deriving (Eq, Ord, Show)
+
+
+------------------------------------------------------------------------
+
+-- | The configuration of a unit.
+--
+--   The cells are relative to a local coordinate system, and will be translated
+--   to the board when the unit is spawned. The local coordinate system of each
+--   cell, like the board's coordinate system, has smaller row numbers in the
+--   "up" direction and smaller column numbers in the "left" direction.
+--
+--   Note that the pivot cell does not have to be a member of the unit.
+--
+data Unit = Unit {
+
+    -- | The unit members.
+    unitMembers :: !(Vector Cell)
+
+    -- | The rotation point of the unit.
+  , unitPivot :: !Cell
+
+  } deriving (Eq, Ord, Show)
+
+
+------------------------------------------------------------------------
+
+-- | The game configuration.
+data Problem = Problem {
+
+    -- | A unique number identifying the problem.
+    problemId :: !Int
+
+    -- | The various unit configurations that may appear in this game.
+    --   There might be multiple entries for the same unit.
+    --
+    --   When a unit is spawned, it will start off in the orientation
+    --   specified in this field.
+    --
+  , problemUnits :: !(Vector Unit)
+
+    -- | The number of cells in a row.
+  , problemWidth :: !Int
+
+    -- | The number of rows on the board.
+  , problemHeight :: !Int
+
+    -- | Which cells start filled.
+  , problemFilled :: !(Vector Cell)
+
+    -- | How many units in the source.
+  , problemSourceLength :: !Int
+
+    -- | How to generate the source and how many games to play.
+  , problemSourceSeeds :: !(Vector Int)
+
+  } deriving (Eq, Ord, Show)
+
+
+------------------------------------------------------------------------
+
+-- | Whether a cell is full or empty.
+data CellStatus = Full | Empty
+  deriving (Eq, Ord, Show)
+
+
+-- | A game board.
+--
+--   The game board consists of hexagonal cells arranged in rows, with the
+--   first row numbered 0. The cells in a row are oriented so that they have
+--   vertices up and down, and edges to the left and right. The first cell in a
+--   row is numbered 0, thus each cell may be identified by a pair of
+--   coordinates (column, row).
+type Board = Array Cell CellStatus
+
+
+------------------------------------------------------------------------
+-- Aeson Instances
+
+instance FromJSON Cell where
+    parseJSON (Object o) = Cell <$> o .: "x"
+                                <*> o .: "y"
+    parseJSON _          = mzero
+
+instance FromJSON Unit where
+    parseJSON (Object o) = Unit <$> o .: "members"
+                                <*> o .: "pivot"
+    parseJSON _          = mzero
+
+instance FromJSON Problem where
+    parseJSON (Object o) = Problem <$> o .: "id"
+                                   <*> o .: "units"
+                                   <*> o .: "width"
+                                   <*> o .: "height"
+                                   <*> o .: "filled"
+                                   <*> o .: "sourceLength"
+                                   <*> o .: "sourceSeeds"
+    parseJSON _          = mzero
+
+
+------------------------------------------------------------------------
+-- Ix Instances
+
+instance Ix Cell where
+    {-# INLINE range #-}
+    range (Cell l1 l2, Cell u1 u2) =
+      [ Cell i1 i2
+      | i1 <- range (l1, u1)
+      , i2 <- range (l2, u2) ]
+
+    {-# INLINE unsafeIndex #-}
+    unsafeIndex (Cell l1 l2, Cell u1 u2) (Cell i1 i2) =
+        unsafeIndex     (l1, u1) i1
+      * unsafeRangeSize (l2, u2)
+      + unsafeIndex     (l2, u2) i2
+
+    {-# INLINE inRange #-}
+    inRange (Cell l1 l2, Cell u1 u2) (Cell i1 i2) =
+         inRange (l1, u1) i1
+      && inRange (l2, u2) i2
