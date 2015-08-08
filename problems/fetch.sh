20fa633674d46cd76c6dee020bfa385776605f23
Jacob Stanley <jacob@stanley.io>
2015-08-08 10:52:48 +1000

    Problem fetch script

---
+++ problems/fetch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+MAX_PROBLEM=23
+PROBLEM_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+for PROBLEM_ID in $(seq 0 $MAX_PROBLEM); do
+  curl \
+    -o $PROBLEM_DIR/problem_${PROBLEM_ID}.json \
+    http://icfpcontest.org/problems/problem_${PROBLEM_ID}.json
+done
