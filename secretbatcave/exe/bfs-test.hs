f83a2dd4e0a24bf350eee24a030c63cd0f6dc783
Christian Marie <christian@ponies.io>
2015-08-09 16:59:56 +1000

    Add breadth first search implementation for finding all locking moves.

---
+++ secretbatcave/exe/bfs-test.hs
@@ -0,0 +1,112 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
+
+module Main where
+
+import Batcave.BFS
+import Batcave.Types
+import qualified Data.Vector as V
+import Data.Set (Set)
+import Data.Maybe
+import qualified Data.Set as Set
+
+
+import           Data.Aeson ((.=))
+import qualified Data.Aeson as A
+import qualified Data.Array as Array
+import qualified Data.ByteString.Lazy.Char8 as L
+import           Data.Maybe (fromMaybe, mapMaybe)
+import           Data.Monoid ((<>))
+import qualified Data.Vector as V
+import           System.Environment (getArgs)
+
+import           Batcave.RunGame (Game(..), initGame, stepGame, currentGameScore)
+import qualified Batcave.Solver.Lucky as Lucky
+import           Batcave.Types
+
+
+b :: Board
+b = emptyBoard (Bounds (Cell 0 0) (Cell 20 20))
+u = singleton $ Unit (V.fromList [Cell 1 0]) (Cell 0 0)
+
+searched :: [Unit]
+searched = Set.toList $ bfs u mempty b
+
+------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+      []       -> putStrLn "Usage: dump PROBLEM"
+      (path:_) -> do
+        problem <- readProblem path
+        L.putStrLn (A.encode (encodeFrames Lucky.solve problem))
+
+------------------------------------------------------------------------
+
+encodeFrames :: (Problem -> Solution) -> Problem -> A.Value
+encodeFrames solve problem@Problem{..} =
+    A.object [ "width"  .= problemWidth
+             , "height" .= problemHeight
+             , "frames" .= frames ]
+  where
+    solution@Solution{..} = solve problem
+
+    frames = V.fromList $ map encodeGame searched
+
+    commands = V.fromList solutionCmds
+
+    game0 = either (\msg -> error ("encodeFrames: " ++ show msg)) id (initGame problem solution)
+
+    board0 = board game0
+
+    step1 = fromJust $ stepGame (V.head commands) game0
+
+    initial_unit :: Unit
+    initial_unit = fromJust (current step1)
+
+    searched :: [Unit]
+    searched = Set.toList $ bfs (singleton initial_unit) mempty board0
+
+    runStep (game1, cmds)
+        | V.null cmds = Nothing
+        | otherwise   = (\g -> (g, (g, V.tail cmds))) <$> stepGame (V.head cmds) game1
+
+encodeGame :: Unit -> A.Value
+encodeGame us =
+    A.object [ "locked"  .= (mapMaybe coord $ thing us)
+             , "current" .= ([] :: [Int])
+             , "score"   .= (42 :: Double) ]
+  where
+    fromBoard = V.fromList
+              . mapMaybe full
+              . Array.assocs
+              . unBoard
+
+    fromCurrent = V.map coord
+                . unitMembers
+
+    takeScore = unGameScore
+              . currentGameScore
+
+    full (cell, Full) = Just (coord cell)
+    full _            = Nothing
+
+    coord (Cell x y) | x < 0 || y < 0 = Nothing
+                    | otherwise       = Just $ V.fromList [x, y]
+
+    thing :: Unit -> [Cell]
+    thing xs = do
+        V.toList $ unitMembers xs
+
+------------------------------------------------------------------------
+
+readProblem :: FilePath -> IO Problem
+readProblem path = do
+    bs <- L.readFile path
+    return (either die id (A.eitherDecode bs))
+  where
+    die msg = error ("readProblem: failed reading json: " ++ msg)
+
---
+++ secretbatcave/src/Batcave/BFS.hs
@@ -0,0 +1,70 @@
+-- | Breadth first search for locking moves
+module Batcave.BFS
+(
+    allLockingPositions
+) where
+
+import           Batcave.Commands
+import           Batcave.Hex
+import           Batcave.Types
+import           Data.Foldable
+import           Data.Monoid
+import           Data.Set         (Set)
+import qualified Data.Set         as Set
+
+-- | Okasaki's, example for queues
+data Queue a = Queue [a] [a]
+
+-- | Append to queue
+put :: a -> Queue a -> Queue a
+put item (Queue ins outs) = Queue (item:ins) outs
+
+-- | Remove from queue
+get :: Queue a -> (a, Queue a)
+get (Queue ins (item:rest)) = (item, Queue ins rest)
+get (Queue ins []) = get (Queue [] (reverse ins))
+
+-- | Create a queue of one element
+singleton :: a -> Queue a
+singleton a = Queue [a] []
+
+-- | Append many things
+append :: Foldable f => f a -> Queue a -> Queue a
+append to_queue queue = foldl' (flip put) queue to_queue
+
+-- | All possible last moves that lock a unit, none of these are valid moves
+-- (they move onto a filled cell or off the board).
+allLockingPositions :: Unit -> Board -> Set Unit
+allLockingPositions start = bfs (singleton start) mempty
+
+-- | Unoptimised BFS over possible moves, accumulates moves that lock the unit,
+-- queues up any valid move that has not already been visited.
+bfs :: Queue Unit -> Set Unit -> Board -> Set Unit
+bfs (Queue [] []) _ _ = mempty
+bfs queue visited board =
+    to_output <> bfs queue'' visited' board
+  where
+    (unit, queue') = get queue
+    (valid_children, invalid_children) = partitionChildren unit board
+    to_queue = Set.difference valid_children visited
+    to_output = Set.difference invalid_children visited
+    visited' = Set.union valid_children $ Set.union invalid_children visited
+    queue'' = append to_queue queue
+
+-- | All legal and illegal moves from the given position
+partitionChildren :: Unit -> Board -> (Set Unit, Set Unit)
+partitionChildren unit board =
+    -- Find all legal moves from current unit
+    let moves = fmap (`applyCommand` unit) commands
+        valid_moves = filter (unitPlaceable board) moves
+        invalid_moves = filter (not . unitPlaceable board) moves
+    in (Set.fromList valid_moves, Set.fromList invalid_moves)
+  where
+    commands =
+        [ Move W
+        , Move E
+        , Move SW
+        , Move SE
+        , Rotate Clockwise
+        , Rotate CounterClockwise
+        ]
